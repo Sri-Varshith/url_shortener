dc8506570ad1e5b0585577051e09fd0835f51c42
Sri Varshith <nvarshith.2007@gmail.com>
2026-06-11 00:25:22 +0530

    updated routing with final routes

--- backend/routes.go
+++ backend/routes.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/go-chi/chi/v5"
 )
 
 func SetupRoutes() *chi.Mux {
 	r := chi.NewRouter()
 
-	r.Get("/", HomeHandler)
+	r.Post("/shorten", CreateShortURL)
+
+	r.Get("/shorten/{code}", GetShortURL)
+
+	r.Put("/shorten/{code}", UpdateShortURL)
+
+	r.Delete("/shorten/{code}", DeleteShortURL)
+
+	r.Get("/shorten/{code}/stats", GetStats)
 
 	return r
-}
-
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "URL Shortener API")
 }
