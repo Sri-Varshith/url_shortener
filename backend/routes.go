07667c14b8e891fdb42a4d96de01e312d2c88944
Sri Varshith <nvarshith.2007@gmail.com>
2026-06-20 01:12:16 +0530

    added cors middleware

--- backend/routes.go
+++ backend/routes.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 func SetupRoutes() *chi.Mux {
 	r := chi.NewRouter()
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{
+			"http://localhost:5173",
+		},
+		AllowedMethods: []string{
+			"GET",
+			"POST",
+			"PUT",
+			"DELETE",
+			"OPTIONS",
+		},
+		AllowedHeaders: []string{
+			"Accept",
+			"Authorization",
+			"Content-Type",
+		},
+	}))
 
 	r.Post("/shorten", CreateShortURL)
 
