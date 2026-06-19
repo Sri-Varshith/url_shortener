2e45857e219c9677d24adfbc3eaba1c0fab5944b
Sri Varshith <nvarshith.2007@gmail.com>
2026-06-20 01:14:02 +0530

    fetch shortcode for url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,25 @@ function App() {
 
     const [url, setUrl] = useState("")
 
+    const handleSubmit = async () => {
+      const response = await fetch(
+        "http://localhost:8000/shorten",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            url: url,
+          }),
+        }
+      )
+
+      const data = await response.json()
+      console.log(data)
+    }
+
+
   return (
     <div className="relative min-h-screen bg-black flex items-center justify-center px-4 overflow-hidden">
 <div className="absolute h-96 w-96 bg-blue-500/20 rounded-full blur-3xl -top-20 -left-20" />
@@ -30,7 +49,7 @@ function App() {
               className="w-full rounded-2xl bg-black/30 border border-white/10 px-5 py-4 text-white outline-none focus:border-blue-500 transition"
           />
 
-          <button className="w-full mt-4 rounded-2xl bg-gradient-to-r from-blue-500 to-purple-500 text-white  font-semibold py-4 hover:scale-[1.02] transition-all duration-300"
+          <button onClick={handleSubmit} className="w-full mt-4 rounded-2xl bg-gradient-to-r from-blue-500 to-purple-500 text-white  font-semibold py-4 hover:scale-[1.02] transition-all duration-300"
           >
             Shorten URL
           </button>
